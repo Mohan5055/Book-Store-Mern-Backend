3de13d0722ad0ccb8cd7a14f0066a718aecd23fa
Anuj Patel <anujpatel5055@gmail.com>
2024-01-24 14:22:03 +0530

    second commit

---
+++ config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://anujpatel5055:Mohan5055@cluster0.b0ohen5.mongodb.net/Book?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
